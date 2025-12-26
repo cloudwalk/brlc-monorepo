1187ab45d4bf5d41b43cc35f8a7f1ec5ca7ee148
Ilia Groshev <ilia.groshev@cloudwalk.io>
2025-12-26 10:05:45 +0000

    dont cleanup old signatures

--- stratus-integration/generate-signatures.bash
+++ stratus-integration/generate-signatures.bash
@@ -5,7 +5,6 @@ set -euo pipefail
 ROOT_DIR="$(pwd)"
 SIGNATURES_DIR="${ROOT_DIR}/signatures"
 
-rm -rf -- "$SIGNATURES_DIR"
 mkdir -p -- "$SIGNATURES_DIR"
 
 export MISE_BACKENDS_SOLIDITY=asdf:diegodorado/asdf-solidity

c41437e3f4febb654a5d411e1f6fa3b83b3c2714
Ilia Groshev <ilia.groshev@cloudwalk.io>
2025-12-29 14:42:02 +0000

    only fail test

--- contracts/token/test/BRLCToken.test.ts
+++ contracts/token/test/BRLCToken.test.ts
@@ -116,7 +116,7 @@ describe("Contract 'BRLCToken'", async () => {
   });
 
   describe("Function 'isBRLCoin'", async () => {
-    it("Returns true", async () => {
+    it.only("Returns true", async () => {
       const { token } = await setUpFixture(deployToken);
       expect(await token.isBRLCoin()).to.eq(true);
     });

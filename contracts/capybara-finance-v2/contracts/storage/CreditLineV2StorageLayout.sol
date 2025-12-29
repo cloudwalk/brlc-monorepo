b5b84605bacfa7460e8b2fac5f303358cf91c470
Evgenii Zaitsev <evgenii@cloudwalk.io>
2025-12-29 19:11:30 +0700

    feat: change the storage namespace

--- contracts/capybara-finance-v2/contracts/storage/CreditLineV2StorageLayout.sol
+++ contracts/capybara-finance-v2/contracts/storage/CreditLineV2StorageLayout.sol
@@ -20,13 +20,12 @@ abstract contract CreditLineV2StorageLayout is ICreditLineV2Types {
      * The value is the same as:
      *
      * ```solidity
-     * string memory id = "cloudwalk.storage.CreditLine";
+     * string memory id = "cloudwalk.storage.CreditLineV2";
      * bytes32 location = keccak256(abi.encode(uint256(keccak256(id) - 1)) & ~bytes32(uint256(0xff));
      * ```
      */
-    // TODO: use storage location id "cloudwalk.storage.CreditLineV2", redeploy the contract
     bytes32 private constant CREDIT_LINE_STORAGE_LOCATION =
-        0xa3fd4b9b32140f4fbbe9f284e8809f89ab5f4a029de5415ce11a80e48f112f00;
+        0x572a836ea1bae82e3ca6e51518ab584428b44d4354f1d91e3b73adf14e348500;
 
     /**
      * @dev Defines the contract storage structure.
@@ -37,7 +36,7 @@ abstract contract CreditLineV2StorageLayout is ICreditLineV2Types {
      * - borrowerStates ---- The mapping of borrower to borrower state.
      * - linkedCreditLine -- The address of the linked credit line.
      *
-     * @custom:storage-location erc7201:cloudwalk.storage.CreditLine
+     * @custom:storage-location erc7201:cloudwalk.storage.CreditLineV2
      */
     struct CreditLineStorage {
         // Slots 1, 2
@@ -52,7 +51,6 @@ abstract contract CreditLineV2StorageLayout is ICreditLineV2Types {
 
     // ------------------ Internal functions ---------------------- //
 
-    // TODO: Rename the function to V2
     /// @dev Returns the storage slot location for the `LendingMarketStorage` struct.
     function _getCreditLineStorage() internal pure returns (CreditLineStorage storage $) {
         assembly {
